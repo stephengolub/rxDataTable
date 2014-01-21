237e389b8156d0e2962adad98eca66d049690c44
Droogans <droogans@gmail.com>
2014-01-21 11:32:54 -0600

    Include documentation.

---
+++ dist/ngdocs/js/docs-setup.js
@@ -0,0 +1,89 @@
+NG_DOCS={
+  "sections": {
+    "guides": "rxDataTable Developer Guides",
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "guides",
+      "id": "rx-data-table",
+      "shortName": "Data Table Directive",
+      "type": "overview",
+      "moduleName": "Data Table Directive",
+      "shortDescription": "Data Table Directive Guide",
+      "keywords": "$scope a1 a2 ability add allow alt angular api app applied apply appropriate attach attribute attributes awesome basic body boolean box bubble call capability catch-all cell check checkbox checkboxes class clause click code coldisplay cols column column-configuration column-display column-object column-presets columns compared complex conditional conditions config configuration created css ctrl curly-braces data data-driven datafield default describing description designed details determine determinewhether dialog directive display displayed displaying div doesn dropdown dt-example1 editable edited editing empty emptyvalue endpoint evaluate evaluation examples expect expected expecting extremely features field fields filter flexbox fluid font format function george guide guides handle header help hidden hide hideable hiding hooks href http icon icons included individual inline inside inverted item js key label link linkfield list list-of-data meet method module multi-column ng-class ng-controller nullable object objects options overview passed popover populate populating post presets pretty prevents promise property proportional provide pulled relative relies reordering require required resolve resolved return row row-key rows rx-data-table rxdatatablemodule save second select selection server set shep single size solution sort sortfield sorting sortinverse space standard string structure style styles styling submit supplied table table-cell team text title tooltip true truthy type type-hint type-hint-array type-hint-boolean type-hint-function type-hint-number type-hint-object type-hint-string typeahead update url user users values var visibility wraps"
+    },
+    {
+      "section": "api",
+      "id": "rxDataTable",
+      "shortName": "rxDataTable",
+      "type": "directive",
+      "moduleName": "rxDataTable",
+      "shortDescription": "Directive that creates a data table with responsive design properties. This",
+      "keywords": "_2 account allows api applied applies apply arguments array attatch attribute attributes bool capabilities character check checkbox checkbox-event checks chkbox chkboxcheckedstatus chkboxkey class clicked code column column-configuration column-display column-multi-sort column-object column-presets column-reordering columnconfiguration columnreordering columns comparisons config configure creates css current currently data data-service-level data-value-key default default-sort definitions design directive directives display displayed dropdown enabled evaluate example expression extended false field fill format function grab guide hidden hold indicate indices item-name items list list-of-data matters multi-sorting non-string numerical object objects order pager paginate pagination paramater parsed passed predicate predicates prepends preset presets properties represent requires responsive row row-key row-style rxdatatable rxitemsperpage rxpaginate selected sort string table takes three title tracking true truthy turns underscore values visibility"
+    },
+    {
+      "section": "api",
+      "id": "rxDataTable",
+      "shortName": "rxDataTable",
+      "type": "directive",
+      "moduleName": "rxDataTable",
+      "shortDescription": "Directive that creates a data table with responsive design properties. This",
+      "keywords": "_2 account allows api applied applies apply arguments array attatch attribute attributes bool capabilities character check checkbox checkbox-event checks chkbox chkboxcheckedstatus chkboxkey class clicked code column column-configuration column-display column-multi-sort column-object column-presets column-reordering columnconfiguration columnreordering columns comparisons config configure creates css current currently data data-service-level data-value-key default default-sort definitions design directive directives display displayed dropdown enabled evaluate example expression extended false field fill format function grab guide hidden hold indicate indices item-name items list list-of-data matters multi-sorting non-string numerical object objects order pager paginate pagination paramater parsed passed predicate predicates prepends preset presets properties represent requires responsive row row-key row-style rxdatatable rxitemsperpage rxpaginate selected sort string table takes three title tracking true truthy turns underscore values visibility"
+    },
+    {
+      "section": "api",
+      "id": "rxDataTable.paginate:Page",
+      "shortName": "Page",
+      "type": "filter",
+      "moduleName": "rxDataTable",
+      "shortDescription": "This is the pagination filter that is used to limit the number of pages",
+      "keywords": "api created displayed filter instance limit list number numbers pager pagetracking paginate pagination rxdatatable service"
+    },
+    {
+      "section": "api",
+      "id": "rxDataTable.paginate:PageTracking",
+      "shortName": "PageTracking",
+      "type": "service",
+      "moduleName": "rxDataTable",
+      "shortDescription": "This is the data service that can be used in conjunction with the pagination",
+      "keywords": "api class conjunction controls createinstance current data determine directive display function generate href initialzed instance item items items_per_page_step itemsizelist itemsperpage iteration list max_per_page min_per_page number object objects pageinit pagenumber pagestoshow pagetracking paginate pagination rxdatatable rxdatatable-paginate-page rxdatatable-paginate-pagetracking-page rxitemsperpage service set setting showall size store stored tables total"
+    },
+    {
+      "section": "api",
+      "id": "rxDataTable.paginate:Paginate",
+      "shortName": "Paginate",
+      "type": "filter",
+      "moduleName": "rxDataTable",
+      "shortDescription": "This is the pagination filter that is used to calculate the division in the",
+      "keywords": "api calculate created current division filter instance items list object pager pagetracking paginate pagination paging rxdatatable service sliced"
+    },
+    {
+      "section": "api",
+      "id": "rxDataTable.paginate:rxItemsPerPage",
+      "shortName": "rxItemsPerPage",
+      "type": "directive",
+      "moduleName": "rxDataTable",
+      "shortDescription": "Directive that takes in a page-tracking object and a label for what to call",
+      "keywords": "allows api box call change defaults directive instance items label list object outputs page-tracking pager paginate restricting rxdatatable select service takes time tracking"
+    },
+    {
+      "section": "api",
+      "id": "rxDataTable.paginate:rxPaginate",
+      "shortName": "rxPaginate",
+      "type": "directive",
+      "moduleName": "rxDataTable",
+      "shortDescription": "Directive that takes in the page tracking object and outputs a page",
+      "keywords": "api controller directive display instance maximum number number-of-pages object outputs page-tracking paginate rxdatatable service switching takes time tracking"
+    }
+  ],
+  "apis": {
+    "guides": false,
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
