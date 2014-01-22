91e1d7f8d858f0dad12ea9d9b515ba0c8dda8009
Droogans <droogans@gmail.com>
2014-01-22 12:57:37 -0600

    jshint fixes.

--- src/scripts/paginate.spec.js
+++ src/scripts/paginate.spec.js
@@ -13,7 +13,7 @@ describe('Pagination', function () {
     describe('Directive: rxPaginate', function () {
         // TODO redo these tests to use class names for finding first, prev, next, last, etc items
 
-        var el, items, scope, compile
+        var el, items, scope, compile,
             validTemplate = '<rx-data-table-paginate page-tracking="pager"></rx-data-table-paginate>';
 
         beforeEach(function() {
@@ -22,7 +22,7 @@ describe('Pagination', function () {
             module('src/templates/rx-data-table-paginate.html');
 
             // Inject in angular constructs
-            inject(function($rootScope, $compile, $filter) {
+            inject(function($rootScope, $compile) {
                 scope = $rootScope.$new();
                 scope.pager = angular.copy(mockPageTracking);
                 compile = $compile;
